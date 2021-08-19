bd8cc5aeacecb67eaad88a8c34308ba6a125770c
Crowdbotics <team@crowdbotics.com>
2021-08-19 07:49:22 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "square_credit_29939Identifier",
+  url: "https://square_credit_29939Identifier.botics.co",
+  api: "https://square_credit_29939Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
